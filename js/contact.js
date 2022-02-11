9f288ddcb64dbc46a7ad6b674cf50264f4849e23
Martin Viola <violamartinalejandro@gmail.com>
2022-02-11 11:00:12 -0300

    alerta envio formulario

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+// var btnSubmit;
+alert("Formulario enviado, te estaremos contactando a la brevedad.");
+
+function confirmacion(evento){
+    alert("Formulario enviado, te estaremos contactando a la brevedad.");
+}
+document.getElementById("#btnSubmit").addEventListener("click",confirmacion)
