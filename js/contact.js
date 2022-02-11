e890b85a719d2e1ef464111973b82e0bbf821f45
MartinViola <98099161+MartinViola@users.noreply.github.com>
2022-02-11 20:27:08 -0300

    Delete js directory

--- js/contact.js
+++
@@ -1,5 +0,0 @@
-function confirmacion(){
-    alert("Formulario enviado, te estaremos contactando a la brevedad.");
-}
-
-document.getElementById("formularioContacto").addEventListener("submit", confirmacion);
